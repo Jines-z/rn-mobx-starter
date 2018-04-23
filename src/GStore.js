0d266f8665e5c8c7dfe809d01085f0bc3dee6de6
郑傲今 <zaj@zhidu.biz>
2018-04-23 12:44:59 +0800

    change mobx introduce

--- src/GStore.js
+++ src/GStore.js
@@ -2,11 +2,12 @@
 
     ** 全局store **
 
-    这里的store通过react的context向全局传递，这里边可以存放例如用户信息，登陆信息，权限等。
-    context相关：http://blog.csdn.net/beverley__/article/details/73847518
+    直接实例化，在 ./App.js 通过 Provider 渗透。
+    在模块内用 @inject('GStore')，将 Store 注入到 props 上。
+    哪里用，哪里 @inject('GStore')。
 
     MobX：可代替redux的状态管理
-    1) useStrict(true)：严格模式，这里的严格模式只约束mobx
+    1) useStrict(true)：严格模式，这里的严格模式只约束mobx(版本小于4)
     2) @observable：定义被观察者，说白了就是定义一个 *可修改* 的 *局部变量*
     3) @action：唯一可以改变观察者的地方
 
@@ -17,6 +18,7 @@
     3) 尽可能晚一点使用间接引用值。
 
     MobX中文文档：http://cn.mobx.js.org/
+    context相关：http://blog.csdn.net/beverley__/article/details/73847518
 
    ====================================================== */
 
